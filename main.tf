9314afc178bd2fcd72c10b03142c4670b9549e17
tran-anne <90490894+tran-anne@users.noreply.github.com>
2024-12-13 04:18:03 +0000

    Correct gitignore_template name

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "github_repository" "dev-repo" {
   name               = "dev-repo"
   description        = "development repo"
   auto_init          = true
-  gitignore_template = "terraform"
+  gitignore_template = "Terraform"
 }
